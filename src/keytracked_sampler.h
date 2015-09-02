a60e2ce5b139f0d051571f447e689eccec430cf9
Oliver Hinds <ohinds@gmail.com>
2015-09-02 14:25:46 -0400

    Add skeleton keytracked sampler.

---
+++ src/keytracked_sampler.h
@@ -0,0 +1,30 @@
+// class to play a sample that's aligned to a keytrack
+
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "types.h"
+
+class KeytrackedSampler {
+
+ public:
+
+  KeytrackedSampler(const std::string& sample_filename);
+
+  ~KeytrackedSampler();
+
+  bool setRelativeRate(float new_relative_rate);
+
+  // get the specified number of audio samples at the specified
+  // position in the keytrack, at the current relative rate
+  shared_ptr<sample_vec> getAudio(nframes_t nframes, float position) const;
+
+ private:
+
+  sample_vec audio;
+  sample_vec stretched_audio;
+
+  float relative_rate;
+};
