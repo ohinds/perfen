4d4455f22c4fe853c2f9224188b37782080a7360
Oliver Hinds <ohinds@gmail.com>
2015-09-02 13:12:07 -0400

    Add skeleton controller class.

---
+++ src/controller.h
@@ -0,0 +1,29 @@
+// class to control audio and midi flow.
+
+#include <string>
+
+#include "types.h"
+
+class Controller {
+
+ public:
+
+  Controller(const std::string& configfile);
+
+  ~Controller();
+
+  bool init();
+
+  bool run();
+
+ private:
+
+  bool parseConfig();
+
+  static int processCallback(nframes_t nframes, void* arg);
+
+  int process(nframes_t nframes);
+
+  std::string configfile;
+
+};
