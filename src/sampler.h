0a65bdf7670bb22c0cebb6fd4c93a30abc0845a3
Oliver Hinds <ohinds@gmail.com>
2015-09-02 14:31:34 -0400

    Add a comment to the add sample function, and change responsibility for file loading to the sampler.

--- src/sampler.h
+++ src/sampler.h
@@ -15,7 +15,9 @@ class Sampler {
 
   ~Sampler();
 
-  bool addSample(midi_event event, const sample_vec &sample);
+  // load a sample from a file, and register it to be played on a
+  // specified MIDI event
+  bool addSample(midi_event event, const std::string &sample_filename;
 
   // process a midi event at a particular velocity, and return any
   // audio that may result from the event
